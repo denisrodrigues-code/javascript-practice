a0cff4bb6a197715b745f57a3bcb99bc4f082c5b
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-13 10:47:55 -0300

    ✨ add: exercicio02 de variaveis e constantes

---
+++ variaveis e constantes/exercicio02.js
@@ -0,0 +1,11 @@
+// Exercício 2: Constantes
+
+// Crie uma constante chamada PI com o valor 3.14159.
+const pi = 3.14159;
+
+// Crie uma variável raio e atribua um valor.
+const raio = 2;
+
+// Calcule a área do círculo usando a fórmula area = PI * raio * raio e mostre o resultado.
+const area = pi * raio * raio;
+console.log(area);
