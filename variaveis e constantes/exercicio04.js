9e5dea4190e4590259584cba293c9b19dba13195
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-13 14:41:24 -0300

    ✨ add: exercicio04 de variaveis e constantes

---
+++ variaveis e constantes/exercicio04.js
@@ -0,0 +1,18 @@
+// Exercício 4: Tipos de dados
+
+// Crie variáveis para armazenar:
+
+// Um número inteiro
+let int = 5;
+
+// Um número decimal
+let dec = 1.5;
+
+// Um texto
+let text = "Denis";
+
+// Um valor boleano (verdadeiro/falso)
+let boll = true;
+
+// Imprima cada variável junto com seu tipo de dado.
+console.log(`${int} é do tipo ${typeof(int)}, ${dec} é do tipo ${typeof(dec)}, ${text} é do tipo ${typeof(text)} e ${boll} é do tipo ${typeof(boll)}`);
