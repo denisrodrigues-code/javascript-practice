5c21a1b5ac92aeed3de97ad415e971f95739a5fc
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-13 10:39:04 -0300

    ✨ add: exercicio01 de variaveis e constantes

---
+++ variaveis e constantes/exercicio01.js
@@ -0,0 +1,10 @@
+// Exercício 1: Declaração e atribuição
+
+// Declare uma variável chamada nome e atribua seu nome.
+const nome = "Denis";
+
+// Declare outra variável chamada idade e atribua sua idade.
+const idade = 35;
+
+// Imprima no console: "Meu nome é [nome] e tenho [idade] anos."
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
