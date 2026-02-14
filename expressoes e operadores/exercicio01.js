420c7eec7daf497761e800e0858a98540e011759
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-14 18:00:12 -0300

    ✨ add: exercicio01 de expressoes e operadores

---
+++ expressoes e operadores/exercicio01.js
@@ -0,0 +1,19 @@
+// Exercício 1: Operações básicas
+
+// Através de dois números mostre:
+
+const num1 = 10;
+
+const num2 = 5;
+
+// Soma
+console.log(`A soma é ${num1 + num2}`);
+
+// Subtração
+console.log(`A subtração é ${num1 - num2}`);
+
+// Multiplicação
+console.log(`A multiplicação é ${num1 * num2}`);
+
+// Divisão
+console.log(`A divisão é ${num1 / num2}`);
