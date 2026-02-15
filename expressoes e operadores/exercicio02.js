8ed8694d204117790dc7d406c8c247d18bfa9fab
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 15:40:53 -0300

    ✨ add: exercicio02 de expressoes e operadores

---
+++ expressoes e operadores/exercicio02.js
@@ -0,0 +1,11 @@
+// Exercício 2: Média Aritmética
+
+// Calcule a média aritmética de três números.
+
+const num1 = 8;
+
+const num2 = 7;
+
+const num3 = 10;
+
+console.log((num1 + num2 + num3) / 3);
