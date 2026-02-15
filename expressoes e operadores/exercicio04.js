b31c42ad50061489fb87705c1b2bd69e91455dc4
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 15:51:59 -0300

    ✨ add: exercicio04 de expressoes e operadores

---
+++ expressoes e operadores/exercicio04.js
@@ -0,0 +1,8 @@
+// Exercício 4: Conversão de Temperatura
+
+// Converta uma temperatura de Celsius para Fahrenheit usando a fórmula:
+// F=(C×9/5)+32
+
+const temp = 33;
+
+console.log((temp * 9 / 5) + 32);
