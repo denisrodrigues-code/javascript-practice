2c697990f85a43e38df471b570d8d098d914fb83
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 22:03:24 -0300

    ✨ add: exercicio04 de condicionais

---
+++ condicionais/exercicio04.js
@@ -0,0 +1,15 @@
+// Exercício 4: Aprovação Escolar
+
+// Leia a nota de um aluno e informe se ele está:
+
+// Aprovado (nota ≥ 7)
+
+// Reprovado (nota < 7)
+
+const grade = 7;
+
+if (grade >= 7) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
