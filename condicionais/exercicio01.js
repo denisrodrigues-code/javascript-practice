66e88938f9d03a51a77ed8333b018896ac19b33e
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 21:43:22 -0300

    ✨ add: exercicio01 de condicionais

---
+++ condicionais/exercicio01.js
@@ -0,0 +1,11 @@
+// Exercício 1: Maioridade
+
+// Leia a idade de uma pessoa e informe se ela é maior ou menor de idade.
+
+const age = 18;
+
+if (age < 18) {
+    console.log("Menor de idade");
+} else {
+    console.log("Maior de idade");
+}
