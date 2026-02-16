fa7b5431d24d6e5045f5a711cf39a39c3a075c67
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 21:53:55 -0300

    ✨ add: exercicio02 de condicionais

---
+++ condicionais/exercicio02.js
@@ -0,0 +1,14 @@
+// Exercício 2: Número Positivo, Negativo ou Zero
+
+// Leia um número e informe sua classificação.
+
+const num = 0;
+
+if (num > 0) {
+    console.log("Número positivo");
+} else if (num < 0) {
+    console.log("Número negativo");
+} else {
+    console.log("Zero");
+}
+
