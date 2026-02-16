893a7af2d2d9ea58df45499515ede53e36ef4aa7
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-15 21:59:07 -0300

    ✨ add: exercicio03 de condicionais

---
+++ condicionais/exercicio03.js
@@ -0,0 +1,11 @@
+// Exercício 3: Par ou Ímpar
+
+// Leia um número inteiro e verifique se ele é par ou ímpar.
+
+const num = 5;
+
+if (num % 2 === 0) {
+    console.log("Número par");
+} else {
+    console.log("Número ímpar");
+}
